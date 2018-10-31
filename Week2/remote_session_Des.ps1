098b019a0772a3f8f4190408192dc5285a5f2721
Dheressa <aaDes.Dheressa@dbhds.virginia.gov>
2018-10-31 16:21:22 -0400

    change2

---
+++ Week2/remote_session_Des.ps1
@@ -0,0 +1,41 @@
+# Create session to the host and enter it (commands will be run on remote host)
+#$Cred = Get-Credential
+$RemoteHost = 'CSHSMGT'
+$Session = New-PSSession -ComputerName $RemoteHost #-Credential $Cred
+Enter-PSSession $Session
+
+# Declare remote credential. Pass DOMAIN\USERNAME
+$RemoteCred = Get-Credential
+
+#Import module1
+Import-Module dbatools
+
+#connect to the instance. Changed -sqlCredential to credential
+Connect-dbainstance -sqlinstance CSHSTESTDB3_1 -credential $RemoteCred
+$target = Connect-dbainstance -sqlinstance CSHSTESTDB3_1 -credential $RemoteCred
+Get-DbaAgDatabase -SqlInstance $target
+
+# 'sqlCredential' is not supported
+Get-DbaAgDatabase -SqlInstance CSHSTESTDB3_1 -sqlcredential $RemoteCred 
+
+#return all job categories
+Get-DbaAgentJobCategory -SqlInstance CSHSTESTDB3_1 -Credential $Cred 
+
+#some options
+#get log
+Get-DbatoolsLog
+
+#get error log
+$error[0] | select * #| out-file "\\cherryhealth.net\shares\MIS\DatabaseMaintenance\error_logs.txt" -append;
+
+# Quick overview of commands
+Start-Process https://dbatools.io/commands
+
+
+UPDATE-Module dbatools
+
+# End the session:
+Exit-PSSession
+
+# Remove the session from the originating host
+Remove-PSSession -Session $Session
